81efeb7664a2bafaad746229bb9d1c6354c4f4ec
skahn <d1215@naver.com>
2022-10-16 01:09:49 +0900

    feat : Add delay rabbit mq

---
+++ rabbitmq-consumer/src/main/java/com/example/rabbitmqconsumer/RabbitmqConsumerApplication.java
@@ -0,0 +1,35 @@
+package com.example.rabbitmqconsumer;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
+
+@SpringBootApplication
+public class RabbitmqConsumerApplication {
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        factory.setPort(5672);
+        factory.setPassword("guest");
+        factory.setUsername("guest");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        channel.queueBind("message","delayed.message","*");
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+            System.out.println(" [x] Received '" + message + "'");
+        };
+        channel.basicConsume("message",true, deliverCallback, consumerTag -> { });
+
+    }
+
+}
